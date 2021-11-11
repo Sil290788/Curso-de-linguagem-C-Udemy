32d3c561502e0036c4703dcb7f255160a12729a0
Silvia Morais <silvia_tholina@hotmail.com>
2021-11-11 19:07:07 -0300

    primeiro projeto

---
+++ Olá, mundo!/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace primeiro_projeto
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, mundo!");
+        }
+    }
+}
