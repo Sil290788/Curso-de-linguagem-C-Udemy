456ba3ec2228e4f342a1259a079b747aed44526a
Silvia Morais <silvia_tholina@hotmail.com>
2021-11-23 13:38:21 -0300

    Nova aula

---
+++ Aula 3/Entrada de dados/Program.cs
@@ -0,0 +1,33 @@
+﻿// Entrada de dados é quando o programa ler os dados que o usuário digitar ou ler de outra fonte.
+
+using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+
+            //string s = Console.ReadLine();    
+            //string[] v = s.Split(' ');  OU A JUNÇÃO DESSAS DUAS LINHAS ABAIXO
+            string[] v = Console.ReadLine().Split(' ');
+            string a = v[0];
+            string b = v[1];
+            string c = v[2];
+
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
