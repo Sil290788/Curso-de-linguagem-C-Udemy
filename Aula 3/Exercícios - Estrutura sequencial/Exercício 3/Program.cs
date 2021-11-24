ea11881d8a573060d85f142f47a9f1a8a93a0901
Silvia Morais <silvia_tholina@hotmail.com>
2021-11-24 16:08:06 -0300

    aula atualizada

---
+++ Aula 3/Exercícios - Estrutura sequencial/Exercício 3/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto 
+de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D). */
+
+using System;
+
+namespace Course
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, D, dif;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            C = int.Parse(Console.ReadLine());
+            D = int.Parse(Console.ReadLine());
+
+            dif = A * B - C * D;
+
+            Console.WriteLine(dif);
+        }
+    }
+}
