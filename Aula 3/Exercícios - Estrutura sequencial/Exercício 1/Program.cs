8bf81d350033d77acfb09474e8c11e74c8ff2207
Silvia Morais <silvia_tholina@hotmail.com>
2021-11-24 15:56:50 -0300

    aula atualizada

---
+++ Aula 3/Exercícios - Estrutura sequencial/Exercício 1/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma 
+mensagem explicativa, conforme exemplos. */
+
+using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            Console.WriteLine("Digite outro número: ");
+
+            int A, B, soma;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            soma = A + B;
+
+            Console.WriteLine(soma);
+        }
+    }
+}
---
+++ Aula 3/Exercícios - Estrutura sequencial/Exercício 2/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro 
+casas decimais conforme exemplos.
+Fórmula da área: area = π . raio2
+Considere o valor de π = 19,63 */
+
+using System;
+using System.Globalization;
+
+namespace Course
+{ 
+    class program
+    {
+        static void Main(string[] args)
+        {
+            double raio = 2.5;
+            double area = Math.PI * (raio * raio);
+
+            Console.WriteLine(area.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
