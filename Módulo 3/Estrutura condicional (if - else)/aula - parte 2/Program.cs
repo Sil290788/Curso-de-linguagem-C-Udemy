84ff17f5b17efa32cd37e4c2ae60efe2fdf2a741
Silvia Morais <silvia_tholina@hotmail.com>
2021-11-26 16:34:42 -0300

    aula atualizada
    Nessa aula foi desenvolvido um programa que mostre a hora atual para entendimento de ENCADEAMENTOS.

---
+++ Módulo 3/Estrutura condicional (if - else)/aula - parte 2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Faça um programa que mostre a hora atual
+
+using System;
+
+namespace estruturaCondicional
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a hora: ");
+
+            int hora = int.Parse(Console.ReadLine());
+
+            if (hora < 12)
+            {
+                Console.WriteLine("Bom dia");
+            }
+            else if (hora < 18)
+            {
+                Console.WriteLine("Boa tarde");
+            }
+            else
+            {
+                Console.WriteLine("Boa noite");
+            }
+        }
+    }
+}
